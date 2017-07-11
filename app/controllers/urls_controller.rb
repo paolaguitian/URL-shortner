7d49e1113fa8ef42ab67ef45d3def8122c977fa9
Paola Guitian <poliguitian@gmail.com>
2017-07-11 09:41:12 -0400

    Control Flow logic update and DRY logic

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -7,23 +7,30 @@ class UrlsController < ApplicationController
   end
 
   def create
+    #create saves og url coming in from sumbit params, also creates randomized
+    #new url then verifies urls have http if not prepends it, then saves
     @url = Url.new(params.require(:url).permit(:original_url)) #CREATES url OBJECT, cfrom sumbit usinf STRONG parmam
     @url.new_url = SecureRandom.urlsafe_base64(8)
 
-    if @url.original_url.include? "https://" || "http://"
-      redirect_to @url.original_url
+    if @url.original_url.include?("https://") || @url.original_url.include?("http://")
+      redirect_and_save
+
     else
       @url.original_url = "https://#{@url.original_url}"
+       redirect_and_save
     end
-
-    if @url.save!
-        redirect_to root_path
-    end
-
   end
 
   def show
-    @url = Url.find_by(new_url: params[:new_url])
-    redirect_to @url.original_url
+    #finds desired object via new url being passed in from route path saved into url
+    @url = Url.find(params[:new_url])
+    redirect_to @url.original_url #redirects to the og url from the object found above
   end
+
+  def redirect_and_save  #DRYS CODE
+    @url.save!
+    redirect_to root_path
+  end
+
+
 end
